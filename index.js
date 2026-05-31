7be5316c36ef6ac6e3028cc4c84deef14c98e22d
mahinzz-1 <mahinproplayer@gmail.com>
2026-05-31 20:59:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -27,15 +27,16 @@ function initWorld() {
 initWorld();
 
 wss.on('connection', (ws) => {
-    console.log('Client connected');
-
     const playerId = Math.random().toString(36).substring(2, 9);
+
+    console.log('Client connected:', playerId);
 
     ws.on('message', (message) => {
         try {
             const packet = JSON.parse(message);
 
             switch (packet.type) {
+
                 case 'Handshake':
                     players[playerId] = {
                         x: 0.5,
@@ -43,17 +44,15 @@ wss.on('connection', (ws) => {
                         z: 0.5,
                         yaw: 0,
                         pitch: 0,
+                        activeBlock: 'grass',
                         name: packet.name
                     };
 
                     ws.send(JSON.stringify({
                         type: 'PlayerJoin',
                         id: playerId,
-                        world,
-                        players: Object.keys(players).map(id => ({
-                            id,
-                            ...players[id]
-                        }))
+                        world: world,
+                        players: Object.keys(players).map(id => ({ id, ...players[id] }))
                     }));
 
                     broadcast({
@@ -72,19 +71,18 @@ wss.on('connection', (ws) => {
 
                 case 'PlayerPosition':
                     if (players[playerId]) {
-                        players[playerId].x = packet.x;
-                        players[playerId].y = packet.y;
-                        players[playerId].z = packet.z;
-                        players[playerId].yaw = packet.yaw;
-                        players[playerId].pitch = packet.pitch;
+                        Object.assign(players[playerId], {
+                            x: packet.x,
+                            y: packet.y,
+                            z: packet.z,
+                            yaw: packet.yaw,
+                            pitch: packet.pitch
+                        });
                     }
                     break;
 
                 case 'PlayerJump':
-                    broadcast({
-                        type: 'PlayerJump',
-                        id: playerId
-                    }, playerId);
+                    broadcast({ type: 'PlayerJump', id: playerId }, playerId);
                     break;
 
                 case 'PlayerPlaceBlock':
@@ -115,25 +113,30 @@ wss.on('connection', (ws) => {
                         players[playerId].activeBlock = packet.activeBlock;
                     }
                     break;
+
+                case 'PlayerSendMessage':
+                    if (players[playerId]) {
+                        broadcast({
+                            type: 'PlayerSendMessage',
+                            id: playerId,
+                            name: players[playerId].name,
+                            message: packet.message
+                        });
+                    }
+                    break;
             }
-        } catch (err) {
-            console.error(err);
+        } catch (e) {
+            console.error(e);
         }
     });
 
     ws.on('close', () => {
         delete players[playerId];
-
-        broadcast({
-            type: 'leave',
-            id: playerId
-        });
-
-        console.log('Client disconnected');
+        broadcast({ type: 'leave', id: playerId });
     });
 });
 
-function broadcast(data, excludeId = null) {
+function broadcast(data) {
     const payload = JSON.stringify(data);
 
     wss.clients.forEach((client) => {
@@ -146,13 +149,10 @@ function broadcast(data, excludeId = null) {
 setInterval(() => {
     broadcast({
         type: 'Tick',
-        players: Object.keys(players).map(id => ({
-            id,
-            ...players[id]
-        }))
+        players: Object.keys(players).map(id => ({ id, ...players[id] }))
     });
 }, 50);
 
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
